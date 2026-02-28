7083d727d788e43bd2a7d8bc3546c02bc88ed6f6
Wulu Knut <wuluknut@qq.com>
2026-02-28 16:53:01 +0800

    test

--- flake.nix
+++ flake.nix
@@ -1,16 +1,6 @@
 {
   description = "NixOS configuration of Wulu Knut";
 
-  nixConfig = {
-    extra-substituters = [
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
